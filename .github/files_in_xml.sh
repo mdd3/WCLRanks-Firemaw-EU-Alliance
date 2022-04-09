f3176d087db113cdab3bdd92859e09aa78f41f5e
mdd3 <a@b>
2022-04-09 11:11:29 +0200

    updated test

--- .github/files_in_xml.sh
+++ .github/files_in_xml.sh
@@ -1,5 +1,6 @@
 echo $(ls)
 for f in $(ls ./WCLRanks/Data/*.lua); do
+	echo "$f"
 	name=$(basename "$f")
 	pattern="<Script\s+file=\"$name\"\s*/>"
 	in_xml=$(grep -E -e $pattern ./WCLRanks/Data/WCLRanks.xml)
@@ -8,6 +9,7 @@ for f in $(ls ./WCLRanks/Data/*.lua); do
 	fi
 done
 for sf in $(grep -E -e "<Script\s+file=\".+\"\s*/>" ./WCLRanks/Data/WCLRanks.xml | sed -E 's/.*\"(.*)\".*/\1/g'); do
+	echo "$sf"
 	if [[ ! -f "./output/$sf" ]]; then
 		exit 1
 	fi
